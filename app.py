aaf6c9f86fc961dad8873aafae2dabd243421bf0
Danster2020 <daniel.bleckert@me.com>
2024-11-12 00:45:58 +0100

    updated port and added battery percentage

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ if __name__ == "__main__":
 
     threading.Thread(target=transcribe_audio,
                      args=(args,), daemon=True).start()
-    socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=5002)
